b7888b9ac8c1432b32c4cae89e1877db9b9f163b
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2021-12-18 09:32:10 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+PROXY_URL = 'https://103.68.251.37:3128/'
+REQUEST_KWARGS = {'proxy_url': 'https://103.68.251.37:3128/'}
+TOKEN = ""1992189735:AAGNrqjMIz5xyc01hG6czNV1vW-8t5sgQlw
+with open('words.txt', 'r', encoding='utf-8') as file:
+    word_list = file.read().splitlines()
