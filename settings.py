d66cdb7d8678ab1c32c2b28fd76023e423efb980
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2021-12-18 09:55:53 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-PROXY_URL = 'https://103.68.251.37:3128/'
-REQUEST_KWARGS = {'proxy_url': 'https://103.68.251.37:3128/'}
+PROXY_URL = 'https://62.141.45.214:3128'
+REQUEST_KWARGS = {'proxy_url': 'https://62.141.45.214:3128/'}
 TOKEN = "1992189735:AAGNrqjMIz5xyc01hG6czNV1vW-8t5sgQlw"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
